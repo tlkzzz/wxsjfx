6079dac6d153a3e1df58a27a30fe76c06e14562c
BearBigBigBig <vip-xiong@foxmail.com>
2017-07-25 11:58:51 +0800

    xrc

--- src/main/java/com/tlkzzz/jeesite/modules/ps/dao/SMemberDao.java
+++ src/main/java/com/tlkzzz/jeesite/modules/ps/dao/SMemberDao.java
@@ -14,5 +14,17 @@ import com.tlkzzz.jeesite.modules.ps.entity.SMember;
  */
 @MyBatisDao
 public interface SMemberDao extends CrudDao<SMember> {
-	
+    /**
+     * 通过ID修改会员手机号
+     * @param id
+     * @param mobile
+     */
+	void updateMobile(String id,String mobile);
+
+    /**
+     * 通过三方登录凭证查询会员信息
+     * @param loginKey
+     * @return
+     */
+    SMember getByKey(String loginKey);
 }
