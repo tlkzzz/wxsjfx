b9abffe84a6c6e302c84eb5aec664e2c0b2e3b8f
BearBigBigBig <vip-xiong@foxmail.com>
2017-07-25 15:24:17 +0800

    xrc

---
+++ src/main/java/com/tlkzzz/jeesite/modules/ps/service/SShopMallService.java
@@ -0,0 +1,15 @@
+package com.tlkzzz.jeesite.modules.ps.service;
+
+import com.tlkzzz.jeesite.common.service.BaseService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created by WT on 2017-07-25.
+ */
+@Service
+@Transactional(readOnly = true)
+public class SShopMallService  extends BaseService {
+
+
+}
