e1c4fa5b6690c7c3554e2f6f99ccc953d7c57011
BearBigBigBig <vip-xiong@foxmail.com>
2017-07-25 15:27:20 +0800

    xrc

--- src/main/java/com/tlkzzz/jeesite/modules/ps/service/SShopMallService.java
+++ src/main/java/com/tlkzzz/jeesite/modules/ps/service/SShopMallService.java
@@ -4,6 +4,8 @@ import com.tlkzzz.jeesite.common.service.BaseService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Random;
+
 /**
  * Created by WT on 2017-07-25.
  */
@@ -11,5 +13,14 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class SShopMallService  extends BaseService {
 
+    public String random(){
+        Random r = new Random();
+        StringBuffer s = new StringBuffer();
+        for(int i=0;i<6;i++) {
+            int num = r.nextInt(10);
+            s.append(num);
+        }
+        return s.toString();
+    }
 
 }
